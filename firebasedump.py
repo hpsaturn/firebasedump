82f4ef87c00a512b035a67f1bbe687b3b096d765
Antonio Vanegas <hpsaturn@gmail.com>
2021-09-21 21:24:41 +0200

    refactored to try to insert commands

--- firebasedump.py
+++ firebasedump.py
@@ -24,15 +24,13 @@ from firebase_admin import db
 from datetime import datetime
 from docopt import docopt
 
-def dumpdb(cert,dbpath,target,output):
 
-  cred = firebase_admin.credentials.Certificate(cert)
-  firebase_admin.initialize_app(cred, {'databaseURL': dbpath})
+def dumpdb(target, output):
 
   now = datetime.now().strftime("%Y%m%d%H%M%S")
 
-  if output==None:
-    out = str(target).replace('/','')
+  if output == None:
+    out = str(target).replace('/', '')
     out = out+str(now)+'.json'
   else:
     out = output+str(now)+'.json'
@@ -43,17 +41,20 @@ def dumpdb(cert,dbpath,target,output):
   except:
       raise IOError('Error in saving data to file!!\n')
 
+
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='0.0.1')
-    debugmode = arguments["--verbose"]
-    cert =      arguments["--credentials"]
-    dbpath =    arguments["--database"]
-    target =    arguments["<dbtarget>"]
-    output =    arguments["<output>"]
-    
-    if debugmode: print(arguments)
+  arguments = docopt(__doc__, version='0.0.1')
+  debugmode = arguments["--verbose"]
+  cert = arguments["--credentials"]
+  dbpath = arguments["--database"]
+  target = arguments["<dbtarget>"]
+  output = arguments["<output>"]
 
-    dumpdb(cert,dbpath,target,output)
+  if debugmode:
+    print(arguments)
 
+  # Firebase database init
+  cred = firebase_admin.credentials.Certificate(cert)
+  firebase_admin.initialize_app(cred, {'databaseURL': dbpath})
 
-
+  dumpdb(target, output)
